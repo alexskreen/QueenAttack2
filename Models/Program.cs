7f6c207a9321f11dbca3bee0f650fd0f4ecb2db0
Student <example@example.com>
2020-02-26 14:11:35 -0800

    add namespace, class, getters, setters, and seperated front and back end logic.

---
+++ Models/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using ChessBoard.Pieces;
+// using System.Collections.Generic;
+
+public class QueenAttack
+{
+  public static void Main()
+  {
+
+    Piece.Question();
+
+  }
+
+}
--- QueenAttack.cs
+++ QueenAttack.cs
@@ -1,19 +1,39 @@
 using System;
 // using System.Collections.Generic;
 
-public class QueenAttack
+namespace ChessBoard.Pieces
 {
-    public static void Main()
+  public class Piece
+  {
+    private int _x;
+    private int _y;
+    public Piece( int x, int y)
     {
+      _x = x;
+      _y = y;
+    }
+    public int GetX()
+    {
+      return _x;
+    }
+    public int GetY()
+    {
+      return _y;
+    }
+    public void SetXCord(int newXCord)
+    {
+      _x = newXCord;
+    }
+    public void SetYCord(int newYCord)
+    {
+      _y = newYCord;
+    }
 
-Question();
-
-    void Question()
+        public static void Question()
     {
         Console.WriteLine("Please enter the X coordinate of the Queen");
         string stringQueenXCord = Console.ReadLine();
         int queenXCord = int.Parse(stringQueenXCord);
-
         Console.WriteLine("Please enter the Y coordinate of the Queen");
         string stringQueenYCord = Console.ReadLine();
         int queenYCord = int.Parse(stringQueenYCord);
@@ -26,15 +46,26 @@ Question();
         string stringPawnYCord = Console.ReadLine();
         int pawnYCord = int.Parse(stringPawnYCord);
 
+        Piece userQueen = new Piece(0, 0);
+        userQueen.SetXCord(queenXCord);
+        userQueen.SetYCord(queenYCord);
+        Console.WriteLine(userQueen.GetX());
+        Console.WriteLine(userQueen.GetY());
+
+        Piece userPawn = new Piece(0, 0);
+        userPawn.SetXCord(pawnXCord);
+        userPawn.SetYCord(pawnYCord);
+        Console.WriteLine(userPawn.GetX());
+        Console.WriteLine(userPawn.GetY());
+
         if (queenXCord == pawnXCord || queenYCord == pawnYCord)
         {
-          Console.WriteLine("Hit on vertical or horizontal movement");
+            Console.WriteLine("Hit on vertical or horizontal movement");
         }
         else if ((Math.Abs(queenXCord - pawnXCord)) == (Math.Abs(queenYCord - pawnYCord)))
         {
-          Console.WriteLine("Hit on diagonal movement");
-
+            Console.WriteLine("Hit on diagonal movement");
         }
     }
-}
-}
+    }
+  }
